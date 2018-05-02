00cdfa45dca03435abc5af5287c83a40c3e0e81c
i.zavadsky <eevanguy1997@gmail.com>
2018-05-02 14:01:39 +0300

    Revert "Revert "implemented temporary photo caching""
    This reverts commit e4ebd72a9f04842680f9fb3335065bfe1314e586.

---
+++ UsersGitHub/UsersGitHub/Model/UserImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersGitHub.Model
+{
+    public class UserImage
+    {
+        public bool IsTimeSaving { get; set; } = false;
+        public string Path { get; set; }
+        public TimeSpan? Time { get; set; }
+    }
+}
