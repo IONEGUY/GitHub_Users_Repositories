3712e373d37b3a9b16fae1aa135524084f531cbb
i.zavadsky <eevanguy1997@gmail.com>
2018-05-02 13:58:34 +0300

    a

--- UsersGitHub/UsersGitHub/ViewModels/SettingsViewModel.cs
+++ UsersGitHub/UsersGitHub/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Akavache;
 using Plugin.Media;
@@ -18,19 +19,27 @@ namespace UsersGitHub.ViewModels
     public class SettingsViewModel : BaseViewModel
     {
         private readonly IPageDialogService dialogService;
-        private readonly ICameraService cameraService;
+        private readonly UserImage userImage;
         private User currentUser;
         private string imageSource;
+        private TimeSpan? time;
         private readonly ICurrentUserService currentUserService;
 
         public ICommand SaveCommand { get; set; }
         public ICommand TakePhotoCommand { get; set; }
         public ICommand GetPhotoCommand { get; set; }
+        public ICommand SetTimeCommand { get; set; }
 
         public User CurrentUser
         {
             get => currentUser;
             set => SetProperty(ref currentUser, value);
+        }
+
+        public TimeSpan? Time
+        {
+            get => time;
+            set => SetProperty(ref time, value);
         }
 
         public string Source
@@ -48,6 +57,7 @@ namespace UsersGitHub.ViewModels
             this.cameraService = cameraService;
             this.dialogService = dialogService;
             this.currentUserService = currentUserService;
+            userImage = new UserImage();
             SetCurrentUser();
             SaveCommand = new Command(Save);
             GetPhotoCommand = new Command(GetPhoto);
@@ -68,28 +78,55 @@ namespace UsersGitHub.ViewModels
             {
                 return;
             }
-            var photo = await cameraService.GetPhoto();
+            var photo = await CrossMedia.Current.PickPhotoAsync();
             Source = photo.Path;
         }
 
         private async void Save()
         {
             var login = currentUserService.User.Login;
-            await BlobCache.UserAccount.Invalidate(login);
             currentUserService.User.UserName = 
                 $"{CurrentUser.FirstName} {CurrentUser.LastName}";
-            currentUserService.User.ImagePath = Source;
+            userImage.Time = Time != new TimeSpan(0,0,0,0) ? Time : null;
+            userImage.Path = Source;
+            currentUserService.User.Image = userImage;
+            await BlobCache.UserAccount.Invalidate(login);
             await BlobCache.UserAccount.InsertObject(login, currentUserService.User);
+            await dialogService.DisplayAlertAsync("","Saved","OK");
         }
 
         private void SetCurrentUser()
         {
             CurrentUser = new User
             {
-                FirstName = currentUserService.User.UserName.Split(' ')[1],
-                LastName = currentUserService.User.UserName.Split(' ')[0],
+                FirstName = currentUserService.User.UserName.Split(' ')[0],
+                LastName = currentUserService.User.UserName.Split(' ')[1],
             };
-            Source = currentUserService.User.ImagePath ?? "";
+            Source = GetUserImage();
+        }
+
+        private string GetUserImage()
+        {
+            var image = currentUserService.User.Image;
+            if (image == null)
+            {
+                Time = new TimeSpan(0, 0, 0, 0);
+                return string.Empty;
+            }
+            if (currentUserService.User.Image.Time == null)
+            {
+                Time = new TimeSpan(0, 0, 0, 0);
+                return image.Path;
+            }
+            var imageTime = image.Time;
+            if (imageTime < DateTime.Now.TimeOfDay)
+            {
+                Time = new TimeSpan(0, 0, 0, 0);
+                userImage.Time = null;
+                return string.Empty;
+            }
+            Time = imageTime;
+            return image.Path;
         }
     }
 }
